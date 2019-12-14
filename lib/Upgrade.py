d91396000485258f12eda4c4b2dacd6a026caaf8
Aμιρ <ajahangirzadeh81@gmail.com>
2019-12-14 19:49:57 +0330

    Update Upgrade.py

--- lib/Upgrade.py
+++ lib/Upgrade.py
@@ -3,11 +3,21 @@
 #Author : @Aμιρ-0x0 (AMJ)
 
 import os , requests
-from shutil import copyfile
-
+import shutil
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
+                shutil.copy2(s, d)
 def upgrade():
         if  os.geteuid() == 0:
-                try:
+                
                         option = input ("Are You Want to Full Upgrade SSF ?[Y/n]")
                         if option == "y" or option == "Y" or option == "yes" or option == "Yes":
                                 try:
@@ -15,20 +25,31 @@ def upgrade():
                                 except:
                                         print ("Please Check Your Internet Connections")
                                 os.system("git clone https://github.com/auip-0x0/sandsploit.git")
-                                src = "sandsploit/*"
+                                src = "sandsploit/"
                                 dst = "/opt/sandsploit/"
-                                copyfile(src, dst)
+                                copytree(src, dst)
                                 """
                                 path = "/opt/sandsploit"
                                 with zipfile.ZipFile("/opt/sandsploit/files.zip", 'r') as zip_ref:
                                         zip_ref.extractall(path)
                                 """
-                                
+			        
+			        os.chmod("/opt/sandsploit/Sandsploit.py",0o755)
+			        shutil.copy("/opt/sandsploit/sandsploit.desktop","/usr/share/applications/sandsploit.desktop")
+                                for root, dirs, files in os.walk(cp):
+                                for d in dirs:
+                                    os.chmod(os.path.join(root, d),0o755)
+                                for f in files:
+                                    os.chmod(os.path.join(root, f), 0o755)
+			        cp = "/opt/sandsploit/module"
+			        for root, dirs, files in os.walk(cp):
+				    for d in dirs:
+				        os.chmod(os.path.join(root, d),0o755)
+				    for f in files:
+				        os.chmod(os.path.join(root, f), 0o755)
                                 print ("Successful. :)")
                         else:
                                 print ("Cancelled.... :( ")
-                except:
-                        print ("Unknown Error!!!")
+                
         else:
                 print ("Please Run Sandsploit with root user to Starting Update...!")
-                
