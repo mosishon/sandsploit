fdd3c8a092040fc0e12dd7f43ebdf740476a91f4
Auip <ajahangirzadeh81@gmail.com>
2019-12-26 00:12:40 +0330

    setup.py Updated

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 #Author @Aμιρ-0x0(AMJ)
 import os  , sys , distro , time , shutil
 from distutils.dir_util import copy_tree
-uname = os.system("uname -a")
-if 'Android' not in uname: 
+"""
+uname = "uname -a"
+unam = os.system(uname)
+android = 'Android'
+if android in unam: 
     if os.geteuid() != 0:
         sys.exit("\n Run only with root access \n")
-
-def get_dis():
-
-    with open("/etc/issue") as f:
-        return f.read().lower().split()[0]
-
+"""
+if os.geteuid() != 0:
+        sys.exit("\n Run only with root access \n")
 def install():
     def slowprint(s):
         for c in s + '\n':
@@ -31,72 +31,56 @@ def install():
                 os.chmod(os.path.join(root, f), 0o755)
         os.system("python3 -m pip install -r docs/requirements.txt")
         print ("Installation completed successfully.....")
-    def termux():
-        copy_tree("project/",path)
-        os.symlink("/data/data/com.termux/files/usr/opt/sandsploit/__init__.py","/data/data/com.termux/files/usr/bin/sandsploit")
-        os.chmod("/opt/sandsploit/__init__.py",0o755)
-        #shutil.copy("/opt/sandsploit/sandsploit.desktop","/usr/share/applications/sandsploit.desktop")
-        cp = "/data/data/com.termux/files/usr/opt/sandsploit/module"
-        for root, dirs, files in os.walk(cp):
-            for d in dirs:
-                os.chmod(os.path.join(root, d),0o755)
-            for f in files:
-                os.chmod(os.path.join(root, f), 0o755)
-        os.system("python3 -m pip install -r docs/requirements.txt")
-        print ("Installation completed successfully.....")
-    if 'Android' not in uname:
-        path = "/opt/sandsploit"
-        exist =  os.path.isdir(path) 
-        if not exist:
-                
-            dis = distro.linux_distribution(full_distribution_name=False)
-            dis = dis[0]
-            archbase = ['arch','Manjaro','arco']
-            debianbase = ['debian','ubuntu','mint','parrot','kali','deepin']
-            void = ['void']
-            bsd = ['freebsd','netbsd','openbsd']
-            if dis in archbase:
-                slowprint("[!] Install the required items ")
-                time.sleep(1)
-                os.system("pacman -S netcat")
-                setup()
-                
-            elif dis in debianbase:
-                slowprint("[!] Install the required items ")
-                time.sleep(1)
-                os.system("apt install netcat")
-                setup()
-                
-            elif dis in void :
-                slowprint("[!] Install the required items ")
-                time.sleep(1)
-                os.system("xbps-install -S netcat")
-                setup()
-                
-            elif dis in bsd:
-                slowprint("[!] Install the required items ")
-                time.sleep(1)
-                os.system("pkg install netcat")
-                setup()
-                
-            else:
-                qs = input ("sandsploit doesn't Support Your dis\nContinue installation? [Y/N] > ")
-                if qs == "y" or qs == "Y" or qs == "yes" or qs == "Yes" :
-                    setup()
+    path = "/opt/sandsploit"
+    exist =  os.path.isdir(path) 
+    if not exist:
+            
+        dis = distro.linux_distribution(full_distribution_name=False)
+        dis = dis[0]
+        archbase = ['arch','Manjaro','arco']
+        debianbase = ['debian','ubuntu','mint','parrot','kali','deepin']
+        void = ['void']
+        bsd = ['freebsd','netbsd','openbsd']
+        if dis in archbase:
+            slowprint("[!] Install the required items ")
+            time.sleep(1)
+            os.system("pacman -S netcat")
+            setup()
+            
+        elif dis in debianbase:
+            slowprint("[!] Install the required items ")
+            time.sleep(1)
+            os.system("apt install netcat")
+            setup()
+            
+        elif dis in void :
+            slowprint("[!] Install the required items ")
+            time.sleep(1)
+            os.system("xbps-install -S netcat")
+            setup()
+            
+        elif dis in bsd:
+            slowprint("[!] Install the required items ")
+            time.sleep(1)
+            os.system("pkg install netcat")
+            setup()
+            
         else:
-            uninstall()
-            install()
+            qs = input ("sandsploit doesn't Support Your dis\nContinue installation? [Y/N] > ")
+            if qs == "y" or qs == "Y" or qs == "yes" or qs == "Yes" :
+                setup()
     else:
-        termux()
+        uninstall()
+        install()
 
 
 
 def uninstall():
-    dst = "/opt/sandsploit/"
-    exist = os.path.isdir(dst) 
+    dirPath = "/opt/sandsploit/"
+    exist = os.path.isdir(dirPath) 
     if exist :
         
-        dirPath = '/opt/sandsploit'
+        
     
         shutil.rmtree(dirPath)
         os.remove('/usr/bin/sandsploit')
@@ -110,14 +94,49 @@ def print_usage():
     print ('''usage :
     [!] - python3 setup.py install            Start installation
     [!] - python3 setup.py uninstall          Start uninstallation''')
+def termux():
+    os.mkdir("/data/data/com.termux/files/usr/opt/sandsploit")
+    path = '/data/data/com.termux/files/usr/opt/sandsploit'
+    copy_tree("project/",path)
+    os.symlink("/data/data/com.termux/files/usr/opt/sandsploit/__init__.py","/data/data/com.termux/files/usr/bin/sandsploit")
+    os.chmod("/opt/sandsploit/__init__.py",0o755)
+    #shutil.copy("/opt/sandsploit/sandsploit.desktop","/usr/share/applications/sandsploit.desktop")
+    cp = "/data/data/com.termux/files/usr/opt/sandsploit/module"
+    for root, dirs, files in os.walk(cp):
+        for d in dirs:
+            os.chmod(os.path.join(root, d),0o755)
+        for f in files:
+            os.chmod(os.path.join(root, f), 0o755)
+    os.system("python3 -m pip install -r docs/requirements.txt")
+    print ("Installation completed successfully.....")
+
+def termuxUn():
+    dirPath = "/data/data/com.termux/files/usr/opt/sandsploit"
+    exist = os.path.isdir(dirPath) 
+    if exist :
+    
+        shutil.rmtree(dirPath)
+        os.remove('/usr/bin/sandsploit')
+        os.remove("/usr/share/applications/sandsploit.desktop")
+        print ("Uninstalled...")
+        return None        
+    else:
+        print ("Sandsploit is not installed.....")
 def main():
+    uname = "uname -a"
     if len(sys.argv) < 2:
         print_usage()
         sys.exit(1)
     elif sys.argv[1] == "install":
-        install()
+        if 'Android' in str(os.system(uname)):
+            termux()
+        else:
+            install()
     elif sys.argv[1] == "uninstall":
-        uninstall()
+        if 'Android' in str(os.system(uname)):
+            termuxUn()
+        else:
+            uninstall()
     else:
         print_usage()
         sys.exit(1)
