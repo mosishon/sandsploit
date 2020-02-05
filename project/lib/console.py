eadb6787668adac0e330fd5d67bcccbfe7e67d81
Aμιρ <ajahangirzadeh81@gmail.com>
2020-02-05 21:48:36 +0330

    Update console.py

--- project/lib/console.py
+++ project/lib/console.py
@@ -147,34 +147,42 @@ exit        Exit From SSF
         print ("\nUnknown Error......")
         print ("Enter ""help"" to show commands....")
         console()
-
 def termux_console():
+    path = None
+    toolpart =None
+    #File lists Function
+    def mp(path):
+        for root,dirs,files in os.walk(path): 
+            for f in files: 
+                print (f)
+    #list Function
+    def list():
+        print ("\nTools\n===============")
+        mp(path)
     try:
-        path = None
-        toolpart =None
-        
-        def mp(path):
-            for root,dirs,files in os.walk(path): 
-                for f in files: 
-                    print (f)
-        def list():
-            print ("\nTools\n===============")
-            mp(path)
+    
         while True:
+            signal.signal(signal.SIGINT,controlc_signal)
+            #Get PWD
             signal.signal(signal.SIGINT,controlc_signal)
             getcwd = os.getcwd()
             getdir = getcwd.split("/")
             pwd =  getdir[-1]
+            #Get LocalHost Name
             plat = platform.node()
+            #Nothing Special :)
             point = "→"
+            #Check Tools Part Directory
             if path == None:
                 None
             else:
                 pth = path.split("/")
                 toolpart = pth[-2]
+            #Promot
             option = input (Fore.RESET+"\n[SSF@%s](%s){%s} %s "%(plat,pwd,toolpart,point))
             
             option2 = option.split(" ")
+            #Conditions
             if option2[0] == "cd":
                 
                 def cd(path):
@@ -193,7 +201,7 @@ def termux_console():
                         else:
                             run = option.split("run ")[1]
                             run2 = path+run
-      
+                            #exec(open(run2).read())
                             exst = os.path.isfile(run2) 
                             if exst:
                                 os.system(run2)
@@ -204,7 +212,7 @@ def termux_console():
             
             elif option2[0] == 'use':
                 try:
-                    check = "/data/data/com.termux/files/usr/opt/sandsploit/module/%s/"%option2[1]
+                    check = "/opt/sandsploit/module/%s/"%option2[1]
                     exist = os.path.isdir(check) 
                     if exist:
                         path = check
@@ -213,7 +221,7 @@ def termux_console():
                     
                 except:
                     print ("Part Not Found")
-            elif option2 == "list":
+            elif option == 'list':
                 if path == None:
                     print("\nTools\n===============")
                     print ("Tools NotFound")
@@ -222,41 +230,43 @@ def termux_console():
 
 
             elif option == 'help':
+                #Menu
                 print ('''
 
 Command     Description
 ========    ============
 banner      Change Banner
 bash        Run Bash Shell
-list        List of tools for each section 
+list        List of tools for each section
 listener    Sniffing Port
 python      Interactive Shell(Debuging Purposes)
 RSMaker     Make Reverse Shell For Desktop Operating Systems
-run         Run Exploits & Tools
+run         Run Tools In modules
 use         Interact With Different Parts of Penetration Testing Tools
 version     Show version of SandSploit
+upgrade     Full Upgrade Freamworks
+update      Update Exploits & Scripts Parts
 exit        Exit From SSF
                 ''')
             elif option == "version":
                 version()
-            #elif option == "update":
-                #update()
-            #elif option == "upgrade":
-                #upgrade()
+            elif option == "update":
+                update()
+            elif option == "upgrade":
+                upgrade()
             elif option == "banner":
                 banner()
             elif option == "RSMaker":
                 RSMaker()
             elif option == "listener":
                 listener()
-                termux_console()
+                console()
             elif option == "exit":
-                break
+                sys.exit()
             else:
                 os.system(option)
 
     except EnvironmentError:
         print ("\nUnknown Error......")
         print ("Enter ""help"" to show commands....")
-        termux_console()
-
+        console()
